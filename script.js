29e84b4e6067749f8d04f02b73f3a83c5033eaeb
cintiaomaas <cintiaomaas@gmail.com>
2023-06-28 19:11:29 -0300

    removendo comentários

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  /*Outra forma de fazer */
-  // if (html.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
-
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
